a70611a233ae7c2f013b803752f0f897063df072
spm <majining@users.noreply.github.com>
2020-10-15 16:04:00 -0500

    Project commit.  Bouncy ball.

---
+++ Assets/Scenes/Level.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+public class Level : MonoBehaviour
+{
+    public GameObject block;
+    
+    [Header("Parameters")]
+    public int width = 20;
+    public int height = 2;
+
+    void Start()
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                Instantiate(block, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+
+        Instantiate(block, new Vector3(0, 1, 0), Quaternion.identity);
+    }
+}
---
+++ Assets/Scenes/PlayerCharacter.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [Header("Movement Parameters")]
+    public float speed = 10f;
+    public float upThrust = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForceMove();
+        Jump();
+        Rocket();
+    }
+
+
+
+//    FixedUpdate
+
+    void ForceMove()
+    {
+        Rigidbody2D body = gameObject.GetComponent<Rigidbody2D>();
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        //transform.position += movement * Time.deltaTime * speed;
+
+        body.AddForce(movement * speed);
+    }
+
+    void Jump()
+    {
+        Rigidbody2D body = gameObject.GetComponent<Rigidbody2D>();
+        Vector3 movement = new Vector3(0f, upThrust, 0f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.down*1.2f, Vector2.down, .1f);
+
+
+        if (Input.GetKeyDown("space") && hit.collider != null && hit.distance < 1)
+        {
+
+    
+
+            print("Object at distance: " + hit.distance);
+            print("Contact at point: " + hit.point);        
+            body.AddForce(Vector2.up * upThrust);
+
+
+           
+        }
+
+        Debug.DrawRay(transform.position, Vector2.down * 1.2f, Color.red);
+
+
+
+    }
+
+
+    void Rocket()
+    {
+        Rigidbody2D body = gameObject.GetComponent<Rigidbody2D>();
+
+        Vector3 movement = new Vector3(0f, Input.GetAxis("Vertical"), 0f);
+
+        body.AddForce(movement * upThrust);
+
+    }
+
+
+
+
+
+}
